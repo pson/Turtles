b3589e2cd7f487b6866b9165b8b3e953d0dad766
Dan Pettersson <gitkraken@pson.pp.se>
2022-08-11 17:55:19 +0200

    Refill slot finished, time to transfer

--- refillSlot.lua
+++ refillSlot.lua
@@ -1,23 +1,23 @@
-function refill(slot)
-    local i
- 
+function refill(slot) 
     turtle.select(slot)
     if turtle.getItemCount() < 8 then
-        for i=4,16,1 do
+        for i=4,16 do
             print("Comparing (", i, ") to (", slot, "): ", turtle.compareTo(i))
             if turtle.compareTo(i) then
                 turtle.select(i)
                 turtle.transferTo(slot)
                 if turtle.getItemCount(slot) > 7 then
                     return true
-                end
+                end -- if 9
                 turtle.select(slot)
-            end
-            return false
-        end
-    end
-    return true
-end
+            end -- if 6
+        end -- for 4
+        print("for loop")
+        return false
+    else -- if 3
+        return true
+    end -- if 3
+end -- function 1
  
 function main()
     refill(2)
